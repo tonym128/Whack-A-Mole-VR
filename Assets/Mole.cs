7599ada0d873a19f8c3bbc1e2accca2246bdfc57
Tony Mamacos <tmamacos@gmail.com>
2018-06-30 13:59:52 +0200

    Initial

---
+++ Assets/Mole.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mole : MonoBehaviour {
+    public float heightVisible = 6f;
+    public float heightHidden = -1f;
+
+    private Vector3 targetPosition;
+    private float speed = 1f;
+
+	// Use this for initialization
+	void Start () {
+        targetPosition = new Vector3(
+                transform.localPosition.x,
+                heightHidden,
+                transform.localPosition.z
+            );
+    }
+
+    // Update is called once per frame
+    void Update () {
+        transform.localPosition = Vector3.Lerp(transform.localPosition, targetPosition, Time.deltaTime * speed);
+	}
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Fire1") || Input.GetKeyDown("space"))
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, transform.forward, out hit))
+            {
+                if (hit.transform.GetComponent<Mole>() != null)
+                {
+                    Debug.Log("Hit a mole");
+                }
+            }
+        }
+    }
+}
